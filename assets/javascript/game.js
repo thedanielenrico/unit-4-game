b39ee6c262f32d7f831df6cdcb52aefc029eca04
thedanielenrico <thedanielenrico@gmail.com>
2019-01-11 09:32:08 -0800

    Add values to all crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,16 +8,16 @@
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
-var score;
+var score = 0;
 var crystal1 = [];
 var crystal2 = [];
 var crystal3 = [];
 var crystal4 = [];
-var randNum;
-
+var randNum = [];
 
 // Game Initial state
 function gameInit() {
+    console.log(randNum);
     var guessMax = 120;
     var guessMin = 19;
     var crystalMax = 12;
@@ -26,8 +26,10 @@ function gameInit() {
     var crystalNum2;
     var crystalNum3;
     var crystalNum4;
-    randNum = Math.floor(Math.random() * (+guessMax - +guessMin) + +guessMin);
-    $("#randomNum").text(randNum);
+    var generateNum;
+    generateNum = Math.floor(Math.random() * (+guessMax - +guessMin) + +guessMin);
+    $("#randomNum").text(generateNum);
+    randNum.push(generateNum);
     crystalNum1 = Math.floor(Math.random() * (+crystalMax - +crystalMin) + +crystalMin);
     crystalNum2 = Math.floor(Math.random() * (+crystalMax - +crystalMin) + +crystalMin);
     crystalNum3 = Math.floor(Math.random() * (+crystalMax - +crystalMin) + +crystalMin);
@@ -36,19 +38,40 @@ function gameInit() {
     crystal2.push(crystalNum2);
     crystal3.push(crystalNum3);
     crystal4.push(crystalNum4);
-    console.log(crystal1);
     console.log(crystal2);
-
+    console.log(crystal1);
 }
 document.body.onload = gameInit();
 
 $("#crystal1").on("click", function () {
-    score = parseInt(crystal1) + totalScore;
+    score += parseInt(crystal1) + totalScore;
     $("#scoreTotal").text(score);
 });
 
 $("#crystal2").on("click", function () {
-    score = parseInt(crystal2) + totalScore;
+    score += parseInt(crystal2) + totalScore;
     $("#scoreTotal").text(score);
 
-})
+});
+$("#crystal3").on("click", function () {
+    score += parseInt(crystal3) + totalScore;
+    $("#scoreTotal").text(score);
+
+});
+$("#crystal4").on("click", function () {
+    score += parseInt(crystal4) + totalScore;
+    $("#scoreTotal").text(score);
+
+});
+if (score === parseInt(randNum)) {
+    alert("you won")
+    wins++;
+    $("#wins").text(wins);
+    gameInit();
+} else if (score > randNum) {
+    alert("you won")
+
+    losses++;
+    $("#losses").text(losses);
+    gameInit();
+}
